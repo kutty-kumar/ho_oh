90d4ca2ce5c78f43d5e2a0140a8e2713f6cd6c3f
Kumar D <kumar.varalakshmi@outlook.com>
2021-08-23 12:12:33 +0530

    shell script fixes

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ export GO_CODE_PATH="github.com/kutty-kumar/ho_oh_go";
 export PWD=$(pwd);
 tag='';
 
-function build() {
+build() {
   [ ! -d "$SOURCE_CODE_PATH" ] && mkdir -p "$SOURCE_CODE_PATH";
   # shellcheck disable=SC2046
   docker run --rm --user $(id -u):$(id -g) -v"${PWD}":"${PWD}" -v"${SOURCE_CODE_PATH}":"${SOURCE_CODE_PATH}" -w"${SOURCE_CODE_PATH}" thethingsindustries/protoc:latest --proto_path="${PWD}" --go_out="${SOURCE_CODE_PATH}" --go-grpc_out="${SOURCE_CODE_PATH}" --grpc-gateway_out="logtostderr=true,allow_delete_body=true:." -I/home/kumard/go/src/ho_oh -I/usr/include/github.com/gogo/protobuf "${PWD}"/core_v1/core.proto "${PWD}"/pikachu_v1/pikachu.proto "${PWD}"/bulbasur_v1/bulbasur.proto "${PWD}"/ditto_v1/ditto.proto "${PWD}"/snorlax_v1/snorlax.proto;
@@ -14,11 +14,11 @@ function build() {
   go mod tidy;
 }
 
-function fill_git_tag() {
+fill_git_tag() {
   tag=$(git describe --always --tags || echo pre-commit);
 }
 
-function push() {
+push() {
   cd "$SOURCE_CODE_PATH"/$GO_CODE_PATH || exit;
   git init --initial-branch=master;
   git remote add origin "$ORIGIN_PATH";
@@ -33,7 +33,7 @@ function push() {
   git push --atomic origin HEAD "$1";
 }
 
-function build_and_push(){
+build_and_push(){
   fill_git_tag;
   echo "$tag";
   build;
