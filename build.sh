285d003ac425957ccb09872a389e7ba582cb577e
Kumar D <kumar.d@cashfree.com>
2021-08-23 11:33:02 +0530

    adding go out

--- build.sh
+++ build.sh
@@ -2,25 +2,27 @@
 
 export SOURCE_CODE_PATH=$HOME/go_proto_files;
 export GO_CODE_PATH="github.com/kutty-kumar/ho_oh_go";
-export PWD=`pwd`;
+export PWD=$(pwd);
 tag='';
 
 build() {
-  [ ! -d $SOURCE_CODE_PATH ] && mkdir -p $SOURCE_CODE_PATH;
-  docker run --rm --user $(id -u):$(id -g) -v${PWD}:${PWD} -v${SOURCE_CODE_PATH}:${SOURCE_CODE_PATH} -w${SOURCE_CODE_PATH} thethingsindustries/protoc:latest --proto_path=${PWD} --go-grpc_out=${SOURCE_CODE_PATH} --grpc-gateway_out="logtostderr=true,allow_delete_body=true:." -I/home/kumard/go/src/ho_oh -I/usr/include/github.com/gogo/protobuf ${PWD}/core_v1/core.proto ${PWD}/pikachu_v1/pikachu.proto ${PWD}/bulbasur_v1/bulbasur.proto ${PWD}/ditto_v1/ditto.proto ${PWD}/snorlax_v1/snorlax.proto;
-  cd $SOURCE_CODE_PATH/$GO_CODE_PATH;
+  [ ! -d "$SOURCE_CODE_PATH" ] && mkdir -p "$SOURCE_CODE_PATH";
+  # shellcheck disable=SC2046
+  docker run --rm --user $(id -u):$(id -g) -v"${PWD}":"${PWD}" -v"${SOURCE_CODE_PATH}":"${SOURCE_CODE_PATH}" -w"${SOURCE_CODE_PATH}" thethingsindustries/protoc:latest --proto_path="${PWD}" --go_out="${SOURCE_CODE_PATH}" --go-grpc_out="${SOURCE_CODE_PATH}" --grpc-gateway_out="logtostderr=true,allow_delete_body=true:." -I/home/kumard/go/src/ho_oh -I/usr/include/github.com/gogo/protobuf "${PWD}"/core_v1/core.proto "${PWD}"/pikachu_v1/pikachu.proto "${PWD}"/bulbasur_v1/bulbasur.proto "${PWD}"/ditto_v1/ditto.proto "${PWD}"/snorlax_v1/snorlax.proto;
+  cd "$SOURCE_CODE_PATH"/$GO_CODE_PATH || exit;
   go mod init $GO_CODE_PATH;
   go mod tidy;
 }
 
 fill_git_tag(){
-  $1=$(git describe --always --tags || echo pre-commit);
+  # shellcheck disable=SC2046
+  "$1"=$(git describe --always --tags || echo pre-commit);
 }
 
 push() {
-  cd $SOURCE_CODE_PATH/$GO_CODE_PATH;
+  cd "$SOURCE_CODE_PATH"/$GO_CODE_PATH || exit;
   git init --initial-branch=master;
-  git remote add origin $ORIGIN_PATH;
+  git remote add origin "$ORIGIN_PATH";
   git config user.name kutty-kumar;
   git config user.email kumar.varalakshmi@outlook.com;
   git add .;
@@ -28,13 +30,13 @@ push() {
   git fetch --all;
   git branch --set-upstream-to=origin/master master;
   git pull --rebase;
-  git tag $1;
-  git push --atomic origin HEAD $1;
+  git tag "$1";
+  git push --atomic origin HEAD "$1";
 }
 
 build_and_push(){
-  fill_git_tag $tag;
-  echo $tag;
+  fill_git_tag "$tag";
+  echo "$tag";
   build;
-  push $tag;
+  push "$tag";
 }
